0cef974707aecbb80eda5ca259d40ea2be076e36
Harshini Kovur <hvkovur@ncsu.edu>
2024-12-14 17:48:09 -0500

    added src folder

--- src/linkedin_api.py
+++ src/linkedin_api.py
@@ -1,4 +1,4 @@
-from linkedin_api import Linkedin
+from src.linkedin_api import Linkedin
 # Authenticate using any Linkedin user account credentials
 api = Linkedin('keerthisurisetty11@gmail.com', 'LifeISgood18!!')
 
